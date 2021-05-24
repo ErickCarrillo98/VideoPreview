277408f4583b4d23443703858874b392fc2e8441
ErickCarrillo98 <82337484+ErickCarrillo98@users.noreply.github.com>
2021-05-24 14:12:31 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//we may need to make use of the .play() and .pause() methods - reference
+
+//We may also need the video to be muted as modern browsers want users to interact with (click) videos for them to start playing.
+
+function play() {
+    var x= document.getElementsByClassName("my-video");
+    my-video.play();
+}
+
+function pause() {
+    my-video.pause();
+}
+
